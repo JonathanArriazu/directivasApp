3979b559f60a6c3e554e31d5215660613d429f34
JoniArriazu <joni.arriazu2@gmail.com>
2022-12-06 13:45:18 -0300

    Change error message color using the directive was applied

--- src/app/shared/directives/error-msg.directive.ts
+++ src/app/shared/directives/error-msg.directive.ts
@@ -1,16 +1,23 @@
-import { Directive, OnInit } from '@angular/core';
+import { Directive, ElementRef, Input, OnInit } from '@angular/core';
 
 @Directive({
   selector: '[error-msg]' //Este es el codigo que le indico a Angular para que implemente mi directiva
 })
 export class ErrorMsgDirective implements OnInit{
 
-  constructor() { 
-    console.log('constructor directive');
+  htmlElement: ElementRef<HTMLElement>;
+  @Input() color: string = 'red'
+
+  constructor( private el: ElementRef<HTMLElement> ) { 
+    this.htmlElement = el;    
   }
 
   ngOnInit(): void {
-    console.log('NgOnInit en la directiva');
+    this.setColor();
+  }
+
+  setColor(): void {
+    this.htmlElement.nativeElement.style.color = this.color;
   }
 
 }
